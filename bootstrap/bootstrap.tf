f4281e10aba15d89826464b771a3a25eb9f96c03
Vicgalan <galcon92@hotmail.com>
2026-02-24 07:39:43 +0000

    chore: remove test tag and sync state

--- bootstrap/bootstrap.tf
+++ bootstrap/bootstrap.tf
@@ -655,7 +655,6 @@ resource "aws_s3_bucket" "terraform_state" {
     {
       Name          = local.tfstate_bucket_name
       resource-type = "state-backend"  # Shared across ALL projects
-      super_test_tag = "SuperTestTag"
     }
   )
 }
